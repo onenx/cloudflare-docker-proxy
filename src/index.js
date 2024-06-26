37c98f645eddf7d9ab4cead6c9c5718e71a42800
aaabbccxxyyzzz <wzlyssn@gmail.com>
2024-06-26 17:17:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.eiice.com": dockerHub,
+  "quay.eiice.com": "https://quay.io",
+  "gcr.eiice.com": "https://gcr.io",
+  "k8s-gcr.eiice.com": "https://k8s.gcr.io",
+  "k8s.eiice.com": "https://registry.k8s.io",
+  "ghcr.eiice.com": "https://ghcr.io",
+  "cloudsmith.eiice.com": "https://docker.cloudsmith.io",
+  "ecr.eiice.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.eiice.com": dockerHub,
 };
 
 function routeByHosts(host) {
